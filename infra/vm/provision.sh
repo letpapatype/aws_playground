279ad3aa5fbe410b72fd76dfb82bbbd0122dbcd0
letpapatype <82685246+letpapatype@users.noreply.github.com>
2024-12-03 09:55:30 -0800

    Update provision.sh

--- infra/vm/provision.sh
+++ infra/vm/provision.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-# install git and curl unzip and nodejs
+
 sudo apt-get install -y -qq --no-install-recommends \
   apt-transport-https \
   apt-utils \
@@ -15,9 +16,7 @@ sudo apt-get install -y -qq --no-install-recommends \
   unzip \
   wget
 
-# install nodejs
 mkdir -p /etc/apt/keyrings
-# grant access to the keyrings folder
 sudo chmod -R 777 /etc/apt/keyrings
 sudo curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=18
@@ -25,20 +24,17 @@ echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.co
 sudo apt-get update
 sudo apt-get install nodejs -y
 
-# Install HashiCorp GPG key
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update -y && sudo apt-get install packer -y
 
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-# Add Docker's official GPG key:
 sudo apt-get update 
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
@@ -46,9 +42,6 @@ echo \
 sudo apt-get update -y
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-# create the user jovann with admin privileges to not have to use sudo
-sudo useradd -m -s /bin/bash jovann
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
